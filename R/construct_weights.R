ecd4f090f5997bef0d8b307c9584a6d24994892e
annaplaksienko <anna.plaksienko@gssi.it>
2022-10-06 14:41:56 +0200

    Addition of a weight construction function

---
+++ R/construct_weights.R
@@ -0,0 +1,38 @@
+#' Construct weights for _jewel_ minimization problem from prior information on vertices degrees.
+#'
+#' Function takes a numerical vector of vertices degrees and constructs weights with the rule \code{W_ij = 1 / sqrt(d_i * d_j)} and then the whole matrix is normilized by the maximum. 
+#
+#'
+#' @param d either one numerical vector or a list of \code{K} numerical vectors 
+#' of length \code{p} with user-provided degrees of vertices for each class. 
+#' If there is only one vector, we assume degrees are the same for all \code{K} classes. 
+#' In that case parameter \code{K} (number of classes) must be provided.
+#' Note that for successful _jewel_ estimation true degrees are not necessary: 
+#' for example, user can provide a vector where known hubs have degree 10 
+#' and the rest of the vertices have degree 1.
+#' @param K number of classes (i.e. datasets, i.e. desired graphs). By default it is length(d).
+#' In length(d) = 1, \code{K} must be provided by the user.
+#'
+#' @return W - a list of \code{K} numeric matrices of the size \code{p} by \code{p}
+#'
+#' @export
+
+construct_weights <- function(d, K = NULL) {
+  if (class(d) == "list" && length(d) > 1) {
+    K <- length(d)
+  } else {
+    if (is.null(K)) {
+      stop("Please provide numebr of classes K.")
+    } else {
+      d_list <- vector(mode = "list", length = K)
+      for (k in 1:K) {
+        d_list[[k]] <- d
+      }
+      d <- d_list
+    }
+  }
+  
+  W <- vector(mode = "list", length = K)
+  W <- lapply(d, function(x) 1 / sqrt(x %*% t(x)))
+  W <- lapply(W, function(x) x / max(x))
+}
